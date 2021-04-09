5da267ae913c67ef53bba3f451bf48de45a29f2b
daniel <dpletzke@gmail.com>
2021-04-09 00:22:50 -0500

    add Pick or ditch icon component

---
+++ app/containers/App/components/Pick.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+
+export function Pick(props) {
+  const { isPick } = props
+  return (
+    <FontAwesomeIcon
+      icon={isPick ? faCheckCircle : faTimesCircle}
+      color={isPick ? 'green' : 'red'}
+    />
+  )
+}
+
+export default Pick
