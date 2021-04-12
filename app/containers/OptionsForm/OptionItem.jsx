cbacac615ab54382ed73d2e13704fdbe9ce0b6c8
daniel <dpletzke@gmail.com>
2021-04-12 04:06:57 -0500

    remove unessary code

--- app/containers/OptionsForm/OptionItem.jsx
+++ app/containers/OptionsForm/OptionItem.jsx
@@ -1,9 +1,8 @@
-// import './OptionItem.scss'
 import React from 'react'
 import classNames from 'classnames'
 
 function OptionItem({ children, clickHandler, label, selected }) {
-  let isSelected = selected === children ? true : false
+  let isSelected = selected === children
 
   const className = classNames('option-item', {
     'option-item--selected': isSelected,
